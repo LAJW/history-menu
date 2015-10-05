746ced80c4c27aba5c720139bb7ff6904aa29d9c
Łukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2015-10-05 20:43:31 +0200

    TYPE(FEAT): Adds timer to WindowFolder

--- source/WindowFolder.js
+++ source/WindowFolder.js
@@ -1,21 +1,40 @@
 "use strict"
 
-class WindowFolder extends Folder {
-	constructor(window) {
-   		typecheck.loose(arguments, {
-			sessionId: String,
-		});
-		super();
-		this.title = "Window (Number of tabs: " + window.tabs.length + ")";
-		for (let tab of window.tabs) {
-			this.insert(new TabButton(tab));
+var WindowFolder = (function () {
+	const template = $({
+		nodeName: "DIV",
+		className: "Timer hidden",
+		childNodes: [$("")]
+	});
+	class WindowFolder extends Folder {
+		constructor(window) {
+			typecheck.loose(arguments, {
+				sessionId: String,
+			});
+			super();
+			this.title = "Window (Number of tabs: " + window.tabs.length + ")";
+			for (let tab of window.tabs) {
+				this.insert(new TabButton(tab));
+			}
+			this._timer = this.DOM.appendChild(template.cloneNode(true)).firstChild;
+			if (window.lastModified)
+				this.timer = relativeTime(window.lastModified * 1000);
+		}
+		click(e) { /*override*/
+			e.preventDefault();
+			Folder.prototype.click.call(this, e);
+			if (e.which == 2 || e.ctrlKey) {
+				Chrome.sessions.restore(this.sessionId, true);
+			}
+		}
+		set timer(value) {
+			typecheck(arguments, [String, undefined]);
+			this._timer.nodeValue = value;
+			this._timer.parentNode.classList.toggle("hidden", !value);
+		}
+		get timer() {
+			return this._timer.nodeValue;
 		}
 	}
-	click(e) { /*override*/
-		e.preventDefault();
-		Folder.prototype.click.call(this, e);
-		if (e.which == 2 || e.ctrlKey) {
-			Chrome.sessions.restore(this.sessionId, true);
-		}
-	}
-}
+	return WindowFolder;
+})();
