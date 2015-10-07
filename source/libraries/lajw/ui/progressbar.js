c79b5e4533546990af28d4189a8fd7dfa57b5388
Łukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2015-10-07 22:25:11 +0200

    TYPE(FEAT): Auto-resizing progressbar

--- source/libraries/lajw/ui/progressbar.js
+++ source/libraries/lajw/ui/progressbar.js
@@ -1,41 +1,49 @@
 "use strict"
+
 var Progressbar = (function () {
 	const template = $({
 		nodeName: "CANVAS",
 		className: "Progressbar",
-		width: 400,
-		height: 400
+		height: 8,
+		width: 400
 	});
 	class Progressbar extends Node {
 		constructor(e) {
 			e = e || {};
 			e.DOM = template.cloneNode(false);
 			super(e);
-			this.DOM.width = 400;
-			this.DOM.height = 8;
 		}
-		fadeIn() { // override
-			var ctx = this.DOM.getContext("2d");
-			ctx.fillStyle = "rgb(64, 129, 244)";
-			var c = [-0.1, -0.2, -0.3];
-			var then = Date.now();
+		fadeIn() { /* override */
+			let ctx = this.DOM.getContext("2d");
+			let c = [-0.1, -0.2, -0.3];
+			let then = Date.now();
+			this.DOM.style.width = "inherit";
 			this._interval = setInterval(function () {
-				var dt = (Date.now() - then) / 1000;
+				if (!this.parent)
+					this.fadeOut(); // BUG - this shouldn't be neccessary
+				let width = this.DOM.offsetWidth | 0;
+				if (this.DOM.width != width)
+					this.DOM.width = width;
+				ctx.fillStyle = "rgb(64, 129, 244)";
+				
+				let dt = (Date.now() - then) / 1000;
 				if (dt > 4 / 1000) {
 					then = Date.now();
-					if (!this.parent)
-						clearInterval(this._interval);
-					ctx.clearRect(0, 0, 400, 10);
-					for (var i = 0; i < 3; i++) {
+					ctx.clearRect(0, 0, width, 10);
+					for (let i = 0; i < 3; i++) {
 						ctx.beginPath();
 						c[i] = (c[i] + dt) % 1;
 						ctx.arc(Math.acos(1 - 2 * c[i]) 
-							/ Math.PI * 400, 2, 4, 4, Math.PI)
+							/ Math.PI * width, 2, 4, 4, Math.PI)
 						ctx.closePath();
 						ctx.fill();
 					}
 				}
 			}.bind(this), 1);
+		}
+		fadeOut() { /* override */
+			clearInterval(this._interval);
+			this._interval = null;
 		}
 	}
 	return Progressbar;
