9f4becb25c72f6cd67801f1423b9e60b1b214186
Lukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2021-06-20 00:34:11 +0100

    REFACTOR: Port MultiButton to typescript

---
+++ source/libraries/lajw/ui/MultiButton.ts
@@ -0,0 +1,14 @@
+import Node from "./Node"
+import Parent from "./Parent"
+import { $ } from "../utils"
+
+const template = $({
+	nodeName: "DIV",
+	className: "MultiButton"
+});
+
+export default class MultiButton extends Parent {
+	constructor(e : { id? : string, children?: Node[] } = {}) {
+		super({ ...e, DOM : template.cloneNode(false) as HTMLElement });
+	}
+}
