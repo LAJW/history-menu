46bedd04b1d16f7e0fac6ba5658490d8f1a48c58
Lukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2021-06-20 00:27:48 +0100

    REFACTOR: Port Layer to TypeScript

---
+++ source/libraries/lajw/ui/Layer.ts
@@ -0,0 +1,26 @@
+import Parent from "./Parent"
+import { $ } from "../utils"
+
+const template = $({
+	nodeName : "DIV",
+	className : "Layer"
+})
+
+export default class Layer extends Parent {
+	_visible : boolean
+	constructor(e : {
+			id? : string
+			visible? : boolean
+		} = {}) {
+		super({ ...e, DOM : template.cloneNode(true) as HTMLElement });
+		this.visible = e.visible === undefined ? true : e.visible;
+	}
+	// bool visible - is this layer visible
+	get visible() {
+		return this._visible;
+	}
+	set visible(value : boolean) {
+		this._visible = value;
+		this.DOM.classList.toggle("visible", value);
+	}
+}
