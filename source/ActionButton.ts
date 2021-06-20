11661a5e90186c08b99dea2329ddb73ecd0c6209
Lukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2021-06-20 01:01:17 +0100

    REFACTOR: Port ActionButton to TypeScript

--- source/ActionButton.ts
+++ source/ActionButton.ts
@@ -1,7 +1,13 @@
 import Button from "./libraries/lajw/ui/Button"
 
 export default class ActionButton extends Button {
-	constructor (e) {
+	constructor (e : {
+			id?: string,
+			icon?: string
+			title?: string
+			tooltip?: string
+			click : (e : MouseEvent) => void
+		}) {
 		super(e);
 		this.click = e.click;
 	}
