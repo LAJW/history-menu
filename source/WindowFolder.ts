5131c6af1a1265520d38ac7f6352f52b9b1b48bf
Lukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2021-06-23 22:24:13 +0100

    FIX: Close the popup after opening a window

--- source/WindowFolder.ts
+++ source/WindowFolder.ts
@@ -40,6 +40,7 @@ export default class WindowFolder extends Folder {
 		e.preventDefault();
 		if (e.button == 1 || e.button == 0 && e.ctrlKey) {
 			Chrome.sessions.restore(this.#sessionId, false);
+			window.close();
 		} else if (e.button == 0) {
 			super.click(e)
 		}
